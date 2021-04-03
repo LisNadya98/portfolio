4a15ba6d9a8346e51156cee1a9b0a20aec61af23
Lis <lis@seam.tv>
2021-04-03 11:45:03 +0800

    [UPD] mobile landing page height

--- src/asset/styles.js
+++ src/asset/styles.js
@@ -100,7 +100,7 @@ const useStyles = makeStyles((theme) => ({
 
     aboutSmall:{
         width: "100vW",
-        height: "700px",
+        height: "800px",
         background: darkBg,
         flexGrow: 1,
         overflow:"visible"
